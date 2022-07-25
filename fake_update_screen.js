fa5c4e51f3c622a49f6f157ad3340b5498c6dafd
Chris <77923728+chriskalv@users.noreply.github.com>
2022-07-25 17:32:23 +0200

    Update fake_update_screen.js

--- fake_update_screen.js
+++ fake_update_screen.js
@@ -1,5 +1,5 @@
 /*###########################################################################
-# Fake_update.js                                                            #
+# Fake Update Screen                                                        #
 # Author: Judge2020                                                         #
 # Edited by: Coderdude112                                                   #
 # This script will show a fake update in fullscreen mode                    #
@@ -9,37 +9,37 @@
 ###########################################################################*/
 
 /* Fake update URLs
-http://fakeupdate.net/vista/    // Windows Vista update
-http://fakeupdate.net/xp/       // Windows XP update
-http://fakeupdate.net/win7/     // Windows 7 update
-http://fakeupdate.net/win8/     // Windows 8 update
-http://fakeupdate.net/win10ue/  // Windows 10 update
-http://fakeupdate.net/win10/    // Windows 10 install
-http://fakeupdate.net/apple/    // OSX update
-http://fakeupdate.net/steam/    // Steam OS update
+http://fakeupdate.net/vista/                         // Windows Vista update
+http://fakeupdate.net/xp/                            // Windows XP update
+http://fakeupdate.net/win7/                          // Windows 7 update
+http://fakeupdate.net/win8/                          // Windows 8 update
+http://fakeupdate.net/win10ue/                       // Windows 10 update
+http://fakeupdate.net/win10/                         // Windows 10 install
+http://fakeupdate.net/apple/                         // OSX update
+http://fakeupdate.net/steam/                         // Steam OS update
 */
 
 // Set the keyboard layout and speed
-layout('de');   // Set keyboard layout to DE
-typingSpeed(0,0);   // Sets the typing speed to super super fast
+layout('de');                                        // Set keyboard layout to DE
+typingSpeed(0,0);                                    // Sets the typing speed to super super fast
 
 // Define all variables, arrays, ect
-var delay_time = 5      // The time to wait before executing the attack in seconds
-var ready = "False"     // Used to make sure that the attack is ready to be executed
-var fake_update = "http://fakeupdate.net/win10ue/"    // The URL to open in fullscreen mode
+var delay_time = 5                                   // The time to wait before executing the attack in seconds
+var ready = "False"                                  // Used to make sure that the attack is ready to be executed
+var fake_update = "http://fakeupdate.net/win10ue/"   // The URL to open in fullscreen mode
 
 function attack() {
     press("GUI r");
     delay(500);
-    type("iexplore -k " + fake_update + "\n");  // Open the fake update website in fullscreen mode
+    type("iexplore -k " + fake_update + "\n");       // Open the fake update website in fullscreen mode
 }
 function check_if_ready() {
     while (ready = "False") {
-        press("SCROLL");    // Press the 'SCROLL LOCK' button
-        result = waitLED(SCROLL,10) // Wait to see if the 'SCROLL LOCK' has changed for 10ms
+        press("SCROLL");                             // Press the 'SCROLL LOCK' button
+        result = waitLED(SCROLL,10)                  // Wait to see if the 'SCROLL LOCK' has changed for 10ms
 
-        if (result.SCROLL) {    // The 'SCROLL LOCK' has changed so we must be connected and ready
-            ready = "True"; // Stop the while loop
+        if (result.SCROLL) {                         // The 'SCROLL LOCK' has changed so we must be connected and ready
+            ready = "True";                          // Stop the while loop
             break;
         }
     }
